4203ed0af1158a05eb766b1783f424e5edf51b62
souvikpaul01 <souvik.paul01@gmail.com>
2020-09-03 23:25:56 +0000

    Modified index and app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 from flask import Flask, render_template
-
+import json
 url = 'https://api.cosmicjs.com/v1/15833b00-e677-11ea-b521-4b536908cb9b/objects?'
 
 headers= {'read_key':'SSL79aDN2ex9V2Bb0S2wOAwJG076H6OE2Yb9VNuTr58bdZNBXW',
@@ -10,16 +10,21 @@ headers= {'read_key':'SSL79aDN2ex9V2Bb0S2wOAwJG076H6OE2Yb9VNuTr58bdZNBXW',
 
 
 response = requests.get(url, params=headers)
-dict = response.json()
 
-#print(dict['objects'])
-#def get_data(): 
- #   for i in dict['objects']:
-  #      x = i['slug']
-   # return x
-    #print(x)
+json_response = response.json()
 
+#dict = json.loads(json_response)
 
+j_dict = json_response['objects']
+'''
+for objects in j_dict:
+    title = objects['title']
+    url = objects['metadata']['hero']['imgix_url']
+         #   return title,images
+'''
+
+title = []
+url = []
 
 app = Flask(__name__)
 
@@ -31,10 +36,13 @@ def main():
 @app.route("/index", methods = ['GET'])
 
 def index():
-    return render_template('index.html',title = dict)
+    for objects in j_dict:
+        title.append(objects['title'])
+        url.append(objects['metadata']['hero']['imgix_url'])
+    
+    return render_template('index.html',title = title, url = url, l = len(j_dict))
 
-#print(response.text.encode('utf8'))
-#print(response.url)
 
 if __name__ == "__main__":
     app.run(debug = True, host ="0.0.0.0", port = 3000)
+
