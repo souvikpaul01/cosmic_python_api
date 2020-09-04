0a58a16d6316f46e3161d15ca145c77104e28088
souvikpaul01 <souvik.paul01@gmail.com>
2020-09-04 01:17:05 +0000

    Fix : Reload Sessions

--- app.py
+++ app.py
@@ -1,37 +1,21 @@
 import requests
 from flask import Flask, render_template
 import json
-url = 'https://api.cosmicjs.com/v1/15833b00-e677-11ea-b521-4b536908cb9b/objects?'
+url_api = 'https://api.cosmicjs.com/v1/15833b00-e677-11ea-b521-4b536908cb9b/objects?'
 
-headers= {'read_key':'SSL79aDN2ex9V2Bb0S2wOAwJG076H6OE2Yb9VNuTr58bdZNBXW',
+params= {'read_key':'SSL79aDN2ex9V2Bb0S2wOAwJG076H6OE2Yb9VNuTr58bdZNBXW',
           'pretty':'false',
           'hide_metafields' : 'true'}
 
-
-
-response = requests.get(url, params=headers)
-json_response = response.json()
-j_dict = json_response['objects']
-
-
-title = []
-url = []
-
 app = Flask(__name__)
 
-@app.route("/")
-
-def main():
-    return "Welcome to the Server"
-
-@app.route("/index", methods = ['GET'])
-
+@app.route("/", methods = ['GET'])
 def index():
-    for objects in j_dict:
-        title.append(objects['title'])
-        url.append(objects['metadata']['hero']['imgix_url'])
-    
-    return render_template('index.html',title = title, url = url, l = len(j_dict))
+    response = requests.get(url_api, params=params)
+    json_response = response.json()
+    posts = json_response['objects']
+
+    return render_template('index.html',posts = posts, l = len(posts) )
 
 
 if __name__ == "__main__":
